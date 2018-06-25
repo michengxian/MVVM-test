6fea1ce65c4c330ea2e8fd532734ec663bc25ecf
MCX <944892236@qq.com>
2018-06-25 09:43:44 +0800

    Initial Commit

---
+++ MVVM-test/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MVVM-test
+//
+//  Created by biostime on 2018/6/25.
+//  Copyright © 2018年 biostime. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ MVVM-test/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  MVVM-test
+//
+//  Created by biostime on 2018/6/25.
+//  Copyright © 2018年 biostime. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
